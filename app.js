c15bfbb124b65f4e169dde4904634fa16a5e3f3d
LeNhan187 <nhanlenguyen1988@gmail.com>
2025-06-28 15:50:22 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const contactRoutes = require('./app/routes/contact.route');
+const contactRouter = require('./app/routes/contact.route');
 const ApiError = require('./app/api-error');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactRoutes);
+app.use("/api/contacts", contactRouter);
 
 // handle 404 response
 app.use((req, res, next) => {
